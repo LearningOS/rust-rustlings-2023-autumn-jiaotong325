3ccece13de14ee2117f5f572fa7e55c0dc53eb37
jiaotong325 <lushen2077@outlook.com>
2023-10-09 19:36:12 +0800

    1009_2

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,10 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    //let c: [i32; 5] = [1, 2, 3, 4, 5];
+    let a: [i32; 100] = [0; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");

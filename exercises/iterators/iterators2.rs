6b66f24a97517a7c1a718f77f8b03cae3ccc9195
jiaotong325 <lushen2077@outlook.com>
2023-10-18 16:57:13 +0800

    1018

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,8 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+use std::f64::consts::E;
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,7 +16,11 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let capitalized_first = first.to_uppercase().collect::<String>();
+            let rest = c.collect::<String>();
+            capitalized_first + &rest
+        }
     }
 }
 
@@ -24,7 +29,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut end = vec![];
+    for word in words{
+        end.push(capitalize_first(word));
+    }
+    return end;
 }
 
 // Step 3.
@@ -32,7 +41,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut end = String::new();
+    for word in words{
+        end.push_str(&capitalize_first(word));
+    }
+    return end;
 }
 
 #[cfg(test)]
--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,10 +3,9 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return the factorial of num
+    // Complete this function to return the factorial of num  //阶乘
     // Do not use:
     // - return
     // Try not to use:
@@ -15,6 +14,7 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    (1..=num).product()    //
 }
 
 #[cfg(test)]
